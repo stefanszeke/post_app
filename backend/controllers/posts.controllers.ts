d2e43e99cf1228d025aad70d3181b88ed5217407
Stefan Szekely <sekestefan@gmail.com>
2022-09-18 14:26:48 +0200

    add: try catch blocks

--- backend/controllers/posts.controllers.ts
+++ backend/controllers/posts.controllers.ts
@@ -3,22 +3,24 @@ import { Database } from "../database/database";
 import AppService from "../services/appService";
 
 export const makePost = async (req: Request, res: Response) => {
-  const {title, text, user_id} = req.body;
+  try {
 
-  const validation = await AppService.postValidation(res, req.body)
-  if(!validation) return;
+    const {title, text, user_id} = req.body;
 
-  const post = { title, text, user_id, score: 0, timestamp: Date.now()};
+    const validation = await AppService.postValidation(res, req.body)
+    if(!validation) return;
 
-  await Database.useMySql("INSERT INTO posts SET ?", post);
-  res.json({message: "Post created"});
+    const post = { title, text, user_id, score: 0, timestamp: Date.now()};
+
+    await Database.useMySql("INSERT INTO posts SET ?", post);
+    res.json({message: "Post created"});
+
+  } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
 
 
 export const getPosts = async (req: Request, res: Response) => {
   try {
-
-    console.log(req.query)
 
     let page: number = +req.query.page!-1;
     if(page < 0) page = 0;
@@ -36,64 +38,74 @@ export const getPosts = async (req: Request, res: Response) => {
 }
 
 export const getUserPosts = async (req: Request, res: Response) => {
-  
-  if(req.query.id) {
-    const getPostWithUsers = `SELECT posts.id, posts.title, posts.text, posts.score, posts.timestamp, users.name as user, users.country FROM posts JOIN users ON (posts.user_id = users.id) WHERE posts.user_id = ? AND posts.id = ?  ORDER BY posts.id DESC;`
-    const post = await Database.useMySql(getPostWithUsers, [req.body.user_id, req.query.id]);
+  try {
+
+    if(req.query.id) {
+      const getPostWithUsers = `SELECT posts.id, posts.title, posts.text, posts.score, posts.timestamp, users.name as user, users.country FROM posts JOIN users ON (posts.user_id = users.id) WHERE posts.user_id = ? AND posts.id = ?  ORDER BY posts.id DESC;`
+      const post = await Database.useMySql(getPostWithUsers, [req.body.user_id, req.query.id]);
+      
+      res.json(post[0]);
+    }
+
+    else {
+
+      let page: number = +req.query.page!-1;
+      if(page < 0) page = 0;
     
-    res.json(post[0]);
-  }
+      let orderBy = req.query.orderBy ||'posts.id' ;
+      let order = req.query.order || 'DESC';
+      let search = req.query.search || "";
 
-  else {
+      const getPostWithUsers = `SELECT posts.id, posts.title, posts.text, posts.score, posts.timestamp, users.name as user, users.country FROM posts JOIN users ON (posts.user_id = users.id) WHERE posts.user_id = ? AND posts.title LIKE '%${search}%' ORDER BY ${orderBy} ${order} LIMIT ?,?`
+      const posts = await Database.useMySql(getPostWithUsers, [req.body.user_id, page*5,5 || 0,5]);
+      
+      res.json(posts);
+    }
 
-    console.log(req.query)
-
-    let page: number = +req.query.page!-1;
-    if(page < 0) page = 0;
-  
-    let orderBy = req.query.orderBy ||'posts.id' ;
-    let order = req.query.order || 'DESC';
-    let search = req.query.search || "";
-
-    const getPostWithUsers = `SELECT posts.id, posts.title, posts.text, posts.score, posts.timestamp, users.name as user, users.country FROM posts JOIN users ON (posts.user_id = users.id) WHERE posts.user_id = ? AND posts.title LIKE '%${search}%' ORDER BY ${orderBy} ${order} LIMIT ?,?`
-    const posts = await Database.useMySql(getPostWithUsers, [req.body.user_id, page*5,5 || 0,5]);
-    
-    res.json(posts);
-  }
-
+  } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
 
 export const updatePost = async (req: Request, res: Response) => {
-  const {title, text, user_id} = req.body;
-  const id = req.params.id;
+  try {
 
-  const validation = await AppService.postValidation(res, req.body)
-  if(!validation) return;
+    const {title, text, user_id} = req.body;
+    const id = req.params.id;
 
-  const post = { title, text };
+    const validation = await AppService.postValidation(res, req.body)
+    if(!validation) return;
 
-  await Database.useMySql("UPDATE posts SET ? WHERE id = ? and user_id = ?", [post, id, user_id]);
-  res.json({message: "Post updated"});
+    const post = { title, text };
+
+    await Database.useMySql("UPDATE posts SET ? WHERE id = ? and user_id = ?", [post, id, user_id]);
+    res.json({message: "Post updated"});
+
+  } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
 
 export const deletePost = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const user_id = req.body.user_id;
+  try {
 
-  await Database.useMySql("DELETE FROM posts WHERE id = ? and user_id = ?", [id, user_id]);
-  res.json({message: "Post deleted"});
+    const id = req.params.id;
+    const user_id = req.body.user_id;
+
+    await Database.useMySql("DELETE FROM posts WHERE id = ? and user_id = ?", [id, user_id]);
+    res.json({message: "Post deleted"});
+
+  } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
 
+
 export const votePost = async (req: Request, res: Response) => {
-  const post_id = req.params.id
-  const {user_id, vote } = req.body;
+  try {
 
-  const userVotes = await Database.useMySql("SELECT upvoted,downvoted FROM users WHERE id = ?", [user_id]);
-  let test:string[] = []
-  let upvotes:string[] = [...userVotes[0].upvoted.split(",")];
-  let downvotes = userVotes[0].downvoted.split(",");
-  let score = 0;
+    const post_id = req.params.id
+    const {user_id, vote } = req.body;
 
+    const userVotes = await Database.useMySql("SELECT upvoted,downvoted FROM users WHERE id = ?", [user_id]);
+    let test:string[] = []
+    let upvotes:string[] = [...userVotes[0].upvoted.split(",")];
+    let downvotes = userVotes[0].downvoted.split(",");
+    let score = 0;
 
     if(vote === "up") {
       if(!upvotes.includes(post_id)) { upvotes.push(post_id); score = 1 }
@@ -113,4 +125,6 @@ export const votePost = async (req: Request, res: Response) => {
     await Database.useMySql("UPDATE users SET upvoted = ?, downvoted = ? WHERE id = ?", [upvotes.join(","), downvotes.join(","), user_id]);
     
     res.json({message: "Post voted"});
+
+    } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
   }
--- backend/controllers/users.controllers.ts
+++ backend/controllers/users.controllers.ts
@@ -6,40 +6,50 @@ import AppService from "../services/appService";
 
 
 export const register = async (req: Request, res: Response) => {
-  const {name, email, country, password, confirm} = req.body;
+  try{ 
 
-  // input validation
-  const validation = await AppService.registerValidation(res, {name, email, country, password, confirm});
-  if(!validation) return;
+    const {name, email, country, password, confirm} = req.body;
 
-  // hashing password
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt);
+    // input validation
+    const validation = await AppService.registerValidation(res, {name, email, country, password, confirm});
+    if(!validation) return;
 
-  // insert user into database
-  const user = {name,email,country,password: hash, upvoted: '0', downvoted: '0'};
-  await Database.useMySql("INSERT INTO users SET ?", user);
-  res.json({message: "User created"});
+    // hashing passwordgit a
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    // insert user into database
+    const user = {name,email,country,password: hash, upvoted: '0', downvoted: '0'};
+    await Database.useMySql("INSERT INTO users SET ?", user);
+    res.json({message: "User created"});
+
+  } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
 
 export const login = async (req: Request, res: Response) => {
-  const {email, password} = req.body;
+  try{ 
+    
+    const {email, password} = req.body;
 
-  // input validation
-  const user = await AppService.loginValidation(res, {email, password});
-  if(!user) return;
-  
-  // generate and send token
-  const token = Authentication.generateToken(user);
-  res.cookie("token", token, {maxAge: 3600000, sameSite: 'none', secure: process.env.NODE_ENV === 'production'});
-  res.cookie("name", user.name, {maxAge: 3600000, sameSite: 'none', secure: process.env.NODE_ENV === 'production'});
-  res.json({message: "User logged in"});
+    // input validation
+    const user = await AppService.loginValidation(res, {email, password});
+    if(!user) return;
+    
+    // generate and send token
+    const token = Authentication.generateToken(user);
+    res.cookie("token", token, {maxAge: 3600000, sameSite: 'none', secure: process.env.NODE_ENV === 'production'});
+    res.cookie("name", user.name, {maxAge: 3600000, sameSite: 'none', secure: process.env.NODE_ENV === 'production'});
+    res.json({message: "User logged in"});
+
+  } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
 
 export const sendVotes = async (req: Request, res: Response) => {
-  const {user_id} = req.body;
-  const userVotes = await Database.useMySql("SELECT upvoted,downvoted FROM users WHERE id = ?", [user_id]);
+  try{ 
 
+    const {user_id} = req.body;
+    const userVotes = await Database.useMySql("SELECT upvoted,downvoted FROM users WHERE id = ?", [user_id]);
+    res.json({upvoted: userVotes[0].upvoted.split(","), downvoted: userVotes[0].downvoted.split(",")} );
 
-  res.json({upvoted: userVotes[0].upvoted.split(","), downvoted: userVotes[0].downvoted.split(",")} );
+ } catch (err) { console.log(err); res.status(500).json({message: "Something went wrong"}) }
 }
